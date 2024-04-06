06a2965c3b996a177ce5b2c8403be151ca49be95
Kenneth Lee <kennethlee.id@gmail.com>
2024-04-06 16:04:59 -0400

    Add ping handling

--- app/server.go
+++ app/server.go
@@ -10,14 +10,33 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	defer listener.Close()
+	fmt.Println("Redis Server is listening on port 6379")
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
+		handleClient(conn)
 	}
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(buf[:n])
+	pongReply := []byte("+PONG\r\n")
+	conn.Write(pongReply)
 }
