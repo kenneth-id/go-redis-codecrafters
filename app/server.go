04fd9d9553a28fc4acd563f4e192a65efe8e8619
Kenneth Lee <kennethlee.id@gmail.com>
2024-04-06 16:16:56 -0400

    Handle multiple pings

--- app/server.go
+++ app/server.go
@@ -30,13 +30,18 @@ func main() {
 
 func handleClient(conn net.Conn) {
 	defer conn.Close()
-	buf := make([]byte, 1024)
-	n, err := conn.Read(buf)
-	if err != nil {
-		fmt.Println(err)
+
+	for {
+		buf := make([]byte, 1024)
+
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(buf[:n])
+		pongReply := []byte("+PONG\r\n")
+		conn.Write(pongReply)
 	}
 
-	fmt.Println(buf[:n])
-	pongReply := []byte("+PONG\r\n")
-	conn.Write(pongReply)
 }
